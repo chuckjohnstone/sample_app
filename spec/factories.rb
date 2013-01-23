796de3760e2fd2efb5cd6e54eec59eea56624538
Chuck Johnston <charles.g.johnston@gmail.com>
2013-01-23 12:37:35 -0800

    finishing section 7.1.3

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name       "Chuck Johnston"
+    email      "michael@example.com"
+    password   "foobar"
+    password_confirmation  "foobar"
+  end
+end
