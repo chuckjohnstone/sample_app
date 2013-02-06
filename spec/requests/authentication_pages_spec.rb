02c671a8d9900711ff37bacbc0288297fefba1cd
Chuck Johnston <charles.g.johnston@gmail.com>
2013-02-05 21:39:53 -0800

    Continue work on Chapter 9

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -5,9 +5,8 @@ describe "Authentication" do
   subject { page }
 
   describe "signin page" do
-    
     before { visit signin_path }
-    
+ 
     describe "with invalid information" do
       before { click_button "Sign in" }
 
@@ -26,10 +25,11 @@ describe "Authentication" do
         fill_in "Email",    with: user.email
         fill_in "Password",  with: user.password
         click_button "Sign in"
-      end
+     end
       
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path )}
       it { should_not have_link('Sign in', href: signin_path) }
       
